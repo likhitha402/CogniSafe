ae6954a6d6490734446e2908479fd394e64e60c7
R.M.Likhitha <rmlikhitha057@gmail.com>
2026-04-09 08:47:18 +0530

    temp rename

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Root from './CogniSafe.jsx'
+import Root from './temp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
